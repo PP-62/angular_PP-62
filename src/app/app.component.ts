8c17f617c432061dad3014c4be22fd3fc94f5ee2
unknown <note-admin@Class34-3.ithub.loc>
2022-01-21 11:31:11 +0300

    counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root", //html tag
+  template: `
+  <h1>hello world, i\`m {{word}}</h1>
+  <input [(ngModel)]="word">
+  <div>{{1}}<div>
+  <button (click)="chng(-1)">-</button> {{cnt}} <button (click)="chng(1)">+</button>
+  `
 })
 export class AppComponent {
-  title = 'angular_PP-62';
+  word: string = "Pear";
+  cnt: number = 0;
+  chng(x:number): void {
+    if(this.cnt>0 || x>0){this.cnt+=x};
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
